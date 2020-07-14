810875d5199939a9a5e426ccb666fed8f09fa048
radhouani mohaned <radhouanimohanad@gmail.com>
2020-07-14 01:47:33 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
 import { Button, Form, FormGroup, Label, Inpute } from 'reactstrap';
+import React, { Component } from 'react';
+// import './App.css';  
 class App extends Component {
   render() {
     return (
-      <Form className="App">
-        <h1>my site </h1>
-        <FormGroup>
-          <div>
-            <label for="name">Name:</label>&nbsp;&nbsp;&nbsp;&nbsp;
-            <input type="text" placeholder="name"></input>
-          </div>
-          <div>
-            <label>prenom:</label>
-            <input type="text" placeholder="prenom"></input>
-          </div>
-
-          <div>
-            <label>email :</label>&nbsp;&nbsp;&nbsp;
-            <input type="text" placeholder="email"></input>
-          </div>
-
-          <div>
-            <label>Comment :</label>
-            <textarea rows="4" cols="50" name="comment" form="usrform" placeholder="say something"></textarea>
-          </div>
-
-        </FormGroup>
-        <div>
-          <button className="btn-lg btn-dark btn-block" type="reset" class="btnr">Reset</button>
-          &nbsp;&nbsp; <button type="submit" class="btnr">submit</button>
-
+      <div className="App">
+        <div className="container">
+          <h1> Radhouani Mohaned</h1>
         </div>
+        <div className="container">
+          <div className="jumbotron">
+            {/* <h1> Todo app</h1> */}
+            <form>
+              <h1 className="initialism">My Form </h1>
+              <FormGroup>
+                <div>
+                  <label for="name">Name:</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input className="form-control" type="text" placeholder="name"></input>
+            
+                </div>
+
+                <div>
+                  <label >prenom:</label>&nbsp;&nbsp;&nbsp;&nbsp;
+            <input className="form-control" type="text" placeholder="prenom"></input>
+                </div>
+
+                <div>
+                  <label>email :</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input className="form-control" type="text" placeholder="email"></input>
+                </div>
+
+                <div>
+                  <label>Comment :</label>
+                  <textarea className="form-control" rows="4" cols="50" name="comment" form="usrform" placeholder="say something"></textarea>
+                </div>
+              </FormGroup>
+              <div>
+                <button type="submit" className="btn btn-primary">submit</button>
+                &nbsp;&nbsp;<button className="btn btn-danger" type="reset" >Reset</button>
 
 
-      </Form>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
     );
   }
 }
